ca7fc4900026cca7d1a33610c1533f9983e103a8
Teklay <teklay@gmail.com>
2023-03-16 20:56:43 +0300

    Add test case for solver class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../solver'
+
+describe Solver do
+    context "Solves for the given methods" do
+        before(:each) do
+            @solver = Solver.new
+        end
+    end
+end
