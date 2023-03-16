0fd652ea7fb5ebf425447bbe0df491f35ce80417
Teklay <teklay@gmail.com>
2023-03-16 21:23:29 +0300

    Add test cases for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -16,5 +16,12 @@ describe Solver do
       expect(@solver.factorial(5)).to eq(120)
       expect(@solver.factorial(6)).to eq(720)
     end
+
+    it 'should display the reverse of the given string' do
+        expect(@solver.reverse('hello')).to eq('olleh')
+        expect(@solver.reverse('world')).to eq('dlrow')
+        expect(@solver.reverse('Tekhlay')).to eq('yalhkeT')
+        expect(@solver.reverse('Tariq')).to eq('qirat')
+        end
   end
 end
